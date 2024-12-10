c63ed92df75010db549f695aa3fa9ff3dc338ea3
Naimul02 <naimulislum39@gmail.com>
2024-12-10 00:43:50 -0800

    project setup done

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+
+import './App.css'
+import Home from './Pages/Home/Home'
+
+function App() {
+  
+
+  return (
+    <div className="m-0 p-0 bg-[#b3261e] h-screen">
+        <Home></Home>
+    </div>
+  )
+}
+
+export default App
