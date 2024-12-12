cd0739b1960e02988993d8e187229e9cbcbd8e04
Naimul02 <naimulislum39@gmail.com>
2024-12-11 18:16:31 -0800

    updated img

--- src/components/Company/Company.jsx
+++ src/components/Company/Company.jsx
@@ -18,15 +18,15 @@ const Company = () => {
     return (
         <div className="flex flex-row gap-28 md:gap-10 lg:gap-28 justify-center md:mx-12 lg:mx-0" data-aos="fade-up">
           <div className="flex flex-col  md:flex-row md:items-center md:justify-center space-y-10 md:gap-10 lg:gap-28 md:space-y-0">
-          <div><img src={img1} className="w-28 h-24" alt="azure"></img></div>
+          <div><img src={img1} className="w-24 h-24" alt="azure"></img></div>
            <div><img src={img2} className="w-24 h-24" alt="github"></img></div>
            <div><img src={img3} className="w-24 h-24" alt="cloudFlare"></img></div>
           </div>
            
            <div className="flex flex-col  md:flex-row md:items-center md:justify-center space-y-10 md:gap-10 lg:gap-28 md:space-y-0">
-           <div><img src={img4} className="w-20 h-20"alt="digitalOcean"></img></div>
-           <div><img src={img5} className="w-20 h-20" alt="googleCloud"></img></div>
-           <div><img src={img6} className="w-28 h-20" alt="linode"></img></div> 
+           <div><img src={img4} className="w-24 h-24"alt="digitalOcean"></img></div>
+           <div><img src={img5} className="w-24 h-24" alt="googleCloud"></img></div>
+           <div><img src={img6} className="w-24 h-24" alt="linode"></img></div> 
            </div>
         </div>
     );
--- src/components/SuperPowers/SuperPowers.jsx
+++ src/components/SuperPowers/SuperPowers.jsx
@@ -25,16 +25,16 @@ const SuperPowers = () => {
 
            <div className="flex flex-row gap-28 md:gap-10 lg:gap-28 justify-center md:mx-12 lg:mx-0 mt-10">
             <div className="flex flex-col  md:flex-row md:items-center md:justify-center space-y-10 md:gap-10 lg:gap-28 md:space-y-0">
-            <div><img src={img3} className="w-28 h-20"alt="Mongodb"></img></div>
+            <div><img src={img3} className="w-24 h-24"alt="Mongodb"></img></div>
             
-           <div><img src={img1} className="w-20 h-20" alt="remix"></img></div>
+           <div><img src={img1} className="w-24 h-24" alt="remix"></img></div>
            <div><img src={img2} className="w-24 h-24" alt="Prisma"></img></div>
             </div>
          
          <div className="flex flex-col  md:flex-row md:items-center md:justify-center space-y-10 md:gap-10 lg:gap-28 md:space-y-0">
-           <div><img src={img5} className="w-20 h-20" alt="paython"></img></div> 
-           <div><img src={img4} className="w-20 h-20" alt=""></img></div>
-           <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfQ4MH3vLA6l72ULn3Up_6undPBcXoERMFcA&s" className="w-16 h-16 rounded-xl" alt="javascript"></img></div>
+           <div><img src={img5} className="w-24 h-24" alt="paython"></img></div> 
+           <div><img src={img4} className="w-24 h-24" alt=""></img></div>
+           <div><img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRfQ4MH3vLA6l72ULn3Up_6undPBcXoERMFcA&s" className="w-24 h-24 rounded-xl" alt="javascript"></img></div>
          
          </div>
         </div> 
